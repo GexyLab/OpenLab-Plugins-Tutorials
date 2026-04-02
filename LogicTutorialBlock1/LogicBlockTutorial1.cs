d3755f05787ce00ed96f03cdfaac85faaf052d4a
Eugenio Liguori <e.liguori@gexy.it>
2026-04-02 19:26:28 +0200

    Initial commit

---
+++ LogicTutorialBlock1/LogicBlockTutorial1.cs
@@ -0,0 +1,27 @@
+﻿using OpenLabSDK.config;
+using OpenLabSDK.error;
+using OpenLabSDK.events;
+using OpenLabSDK.instruments;
+using OpenLabSDK.plugin;
+using OpenLabSDK.ui;
+
+namespace LogicBlockTutorial1
+{
+    public class LogicBlockTutorial1 : LogicBlock
+    {
+        public LogicBlockTutorial1(IErrorManager _errorManager, IPluginsManager _pluginsManager, IEventsManager _eventsManager, IWindowsManager _windowsManager, InstrumentDefinition instrumentDefinition) : base(_errorManager, _pluginsManager, _eventsManager, _windowsManager, instrumentDefinition)
+        {
+        }
+
+        public override void run()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+    
